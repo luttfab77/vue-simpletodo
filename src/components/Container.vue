b0320ab47b5511a867dd33e24efc2d61a5293840
luttfab77 <72748201+luttfab77@users.noreply.github.com>
2022-11-04 09:28:04 +0100

    simple todolist

---
+++ src/components/Container.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <input type="text" v-model="msg" />
+    <button @click="Create">Create</button>
+    <TodoList :TodoLists="data" :Delete="Delete" />
+  </div>
+</template>
+
+<script>
+import TodoList from './ToDo.vue';
+import { ref } from 'vue';
+export default {
+  components: {
+    TodoList,
+  },
+  setup() {
+    let data = ref([
+      { name: 'eating' },
+      { name: 'drinking' },
+      { name: 'sleeping' },
+    ]);
+    function Create() {
+      data.value.push({ name: this.msg });
+    }
+    function Delete(idx) {
+      data.value.splice(idx, 1);
+    }
+    return {
+      data,
+      Create,
+      Delete,
+    };
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.container {
+  font-size: 20px;
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+  margin-left: auto;
+  margin-right: auto;
+}
+input {
+  font-size: 20px;
+  border-radius: 5px;
+}
+button {
+  border-radius: 5px;
+  font-size: 20px;
+  background-color: green;
+}
+</style>
